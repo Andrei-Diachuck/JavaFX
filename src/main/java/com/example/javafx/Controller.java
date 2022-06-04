2ba22e1bd680c180d470adbb75714660c998500a
Andrei-Diachuck <58138518+Andrei-Diachuck@users.noreply.github.com>
2022-06-04 18:38:38 +0300

    hw4

---
+++ src/main/java/com/example/javafx/Controller.java
@@ -0,0 +1,26 @@
+package com.example.javafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    private TextField userAnswer;
+    @FXML
+    private TextArea historyArea;
+    
+    
+    public void clickCheckButton(ActionEvent actionEvent) {
+        String answer = userAnswer.getText();
+        if (answer.isBlank()){
+            return;
+        }
+        String text = String.format(answer);
+        historyArea.appendText(text + "\n");
+        userAnswer.clear();
+        userAnswer.requestFocus();
+    }
+}
